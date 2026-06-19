69a4f58f4bad043a0c121efd21bb5e55bafcb5ff
huuhoan229 <hhoan.cv@gmail.com>
2026-06-19 15:01:44 +0700

    Fix early AVAudioSession initialization crash by using UIApplicationDelegateAdaptor

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -1,18 +1,22 @@
 import SwiftUI
 import UIKit
-import UserNotifications
 import AVFoundation
 
-@main
-struct MiuBonSubApp: App {
-    init() {
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         do {
             try AVAudioSession.sharedInstance().setCategory(.playback, mode: .moviePlayback)
             try AVAudioSession.sharedInstance().setActive(true)
         } catch {
             print("Failed to set audio session category: \(error)")
         }
+        return true
     }
+}
+
+@main
+struct MiuBonSubApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
     var body: some Scene {
         WindowGroup {
@@ -20,4 +24,3 @@ struct MiuBonSubApp: App {
         }
     }
 }
-
